ea33f221fef67fbb4da9f0584399a1b4f92ac598
alex6446 <alexeymedenitskiy@gmail.com>
2020-10-12 17:28:59 +0300

    Finish him

--- Stack/Stack.cs
+++ Stack/Stack.cs
@@ -9,67 +9,67 @@ namespace My {
 
     public class Stack {
 
-        private class Node {
-            public float data;
-            public Node next;
+        public class Node {
+            public float Data { get; set; }
+            public Node Next { get; set; }
         }
-        private Node begin;
-        Node Begin { get; set; }
+        
+        public Node Begin { get; set; }
 
         public Stack() { }
         
         public Stack(float[] arr) {
             foreach (var item in arr) {
                 Node temp = new Node();
-                temp.data = item;
-                temp.next = begin;
-                begin = temp;
+                temp.Data = item;
+                temp.Next = Begin;
+                Begin = temp;
             }
         }
 
         public Stack(Stack s) {
-            Node snode = s.begin;
+            Node snode = s.Begin;
             while (snode != null) {
                 Node temp = new Node();
-                temp.data = snode.data;
-                temp.next = begin;
-                begin = temp;
-                snode = snode.next;
+                temp.Data = snode.Data;
+                temp.Next = Begin;
+                Begin = temp;
+                snode = snode.Next;
             } 
         }
 
         public void Push(float value) {
             Node temp = new Node();
-            temp.data = value;
-            temp.next = begin;
-            begin = temp;
+            temp.Data = value;
+            temp.Next = Begin;
+            Begin = temp;
         }
 
         public float Pop() {
-            if (begin == null)
+            if (Begin == null)
                 throw new NullReferenceException();
-            float res = begin.data;
-            begin = begin.next;
+            float res = Begin.Data;
+            Begin = Begin.Next;
             return res;
         }
 
         public float Peek() {
-            if (begin == null)
+            if (Begin == null)
                 throw new NullReferenceException();
-            return begin.data;
+            return Begin.Data;
         }
 
         public override bool Equals(object obj) {
             if (obj == null || GetType() != obj.GetType())
                 return false;
             Stack s = (Stack)obj;
-            Node tnode = begin;
-            Node snode = s.begin;
+            Node tnode = Begin;
+            Node snode = s.Begin;
             while (tnode != null && snode != null) {
-                if (tnode.data != snode.data)
+                if (tnode.Data != snode.Data)
                     return false;
-                tnode = tnode.next;
-                snode = snode.next;
+                tnode = tnode.Next;
+                snode = snode.Next;
             }
             if (tnode != null || snode != null)
                 return false;
@@ -77,21 +77,21 @@ namespace My {
         }
 
         public void Print() {
-            Node tnode = begin;
+            Node tnode = Begin;
             while (tnode != null) {
-                Console.WriteLine("{0} ", tnode.data);
-                tnode = tnode.next;
+                Console.WriteLine("{0} ", tnode.Data);
+                tnode = tnode.Next;
             }
         }
 
         public void Input() {
-            begin = null;
+            Begin = null;
             int N = int.Parse(Console.ReadLine());
             for (int i = 0; i < N; ++i) {
                 Node temp = new Node();
-                temp.data = float.Parse(Console.ReadLine());
-                temp.next = begin;
-                begin = temp;
+                temp.Data = float.Parse(Console.ReadLine());
+                temp.Next = Begin;
+                Begin = temp;
             }
         }
 
